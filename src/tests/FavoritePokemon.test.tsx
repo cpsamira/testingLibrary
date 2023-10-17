6faf361ed0e31b4396ab3ecbf577ba90771d7499
Samira Silva <samirasilvamjj@gmail.com>
2023-10-17 17:11:50 -0300

    requisito 03

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('testa se é exibido texto de erro caso a pessoa não tenha Pokémon favorito', () => {
+  renderWithRouter(<App />, { route: '/favorites' });
+
+  const noHaveFavorite = screen.getByText('No favorite Pokémon found');
+
+  expect(noHaveFavorite).toBeInTheDocument();
+});
+
+test('testa se são exibidos apenas os Pokémons favoritados', async () => {
+  renderWithRouter(<App />);
+
+  const moreDetails = screen.getByText('More details');
+  await userEvent.click(moreDetails);
+
+  const isFavorite = screen.getByText('Pokémon favoritado?');
+  await userEvent.click(isFavorite);
+
+  const favoritePokemon = screen.getByText('Favorite Pokémon');
+  await userEvent.click(favoritePokemon);
+
+  expect(screen.getByText('Pikachu')).toBeInTheDocument();
+});
