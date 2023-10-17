5a8c62b97fdfdff5d0d5cde25d9335acf4f3f474
Samira Silva <samirasilvamjj@gmail.com>
2023-10-17 15:33:57 -0300

    requisito 02

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,39 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
+
+describe('testa componente <About.tsx />', () => {
+  test('Testa se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const pokedexInfo = screen.getByText('What does this app do?');
+
+    expect(pokedexInfo).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const title = screen.getByRole('heading', { level: 2 });
+
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const paragraphOne = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon.');
+    const paragrpgTwo = screen.getByText('One can filter Pokémon by type, and see more details for each one of them.');
+
+    expect(paragraphOne).toBeInTheDocument();
+    expect(paragrpgTwo).toBeInTheDocument();
+  });
+
+  test('Testa se a página contém a imagem correta', () => {
+    renderWithRouter(<About />);
+
+    const pokedexImage = screen.getByAltText(/Pokédex/);
+
+    expect(pokedexImage).toBeInTheDocument();
+  });
+});
